752b4545bc7e9a5c4646802184151907bdf1ca5f
Pedro Menegatti <pedro.menegatti@centauro.com.br>
2019-05-22 07:14:42 -0300

    mapear elementos

---
+++ features/step_definitions/links_e_botoes.rb
@@ -0,0 +1,12 @@
+Quando("clico em botoes.") do
+  visit '/'
+  click_on('Começar Automação Web') #click_link_or_button sao identicos
+  visit '/buscaelementos/botoes'
+  click_button(class: 'btn waves-light')
+  find('a[onclick="ativaedesativa2()"]').click
+  find('a[onclick="ativaedesativa2()"]').double_click
+  find('a[onclick="ativaedesativa3()"]').right_click
+  visit '/'
+  click_link('Desenvolvido por Bruno Batista')
+  sleep(5)
+end
