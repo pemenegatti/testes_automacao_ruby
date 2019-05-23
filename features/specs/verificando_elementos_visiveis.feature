efa2b64bed3f739e31941d2217e7aabba41e050d
Pedro Menegatti <pedro.menegatti@centauro.com.br>
2019-05-23 12:44:40 -0300

    testes branches

---
+++ features/specs/verificando_elementos_visiveis.feature
@@ -0,0 +1,8 @@
+#language: pt
+
+@verificando_elementosI
+Funcionalidade: Verificando elementos visiveis na tela
+
+Cenario: Verificar elementos visivel com sucesso.
+Quando clico no botão.
+Então verifico se o texto apareceu na tela com sucesso.
---
+++ features/step_definitions/verificando_elementos_visiveis.rb
@@ -0,0 +1,18 @@
+Quando("clico no botão.") do
+  visit '/'
+  #click_on('Começar Automação Web')
+  find('a[href="/treinamento/home"]').click
+  find('a', text: 'Busca de elementos').click
+  find('a[href="/buscaelementos/botoes"]').click
+  find('#teste').click
+  sleep(2)
+end
+  
+Então("verifico se o texto apareceu na tela com sucesso.") do
+  #@texto = find('#div1')
+  #expect(@texto.text).to eq 'Você Clicou no Botão!'
+  page.assert_text(text, 'Você Clicou no Botão!')
+  page.has_text?('Você Clicou no Botão!')
+  have_text('Você Clicou no Botão!')
+
+end
