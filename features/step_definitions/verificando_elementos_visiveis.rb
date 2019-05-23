616f01c3646940a82e4d8b5029839d9a7716e692
Pedro Menegatti <pedro.menegatti@centauro.com.br>
2019-05-23 17:19:48 -0300

    negativas de elementos

--- features/step_definitions/verificando_elementos_visiveis.rb
+++ features/step_definitions/verificando_elementos_visiveis.rb
@@ -2,7 +2,7 @@ Quando("clico no botão.") do
   visit '/'
   #click_on('Começar Automação Web')
   find('a[href="/treinamento/home"]').click
-  find('a', text: 'Busca de elementos').click
+  find("a", text: 'Busca de elementos').click
   find('a[href="/buscaelementos/botoes"]').click
   find('#teste').click
   sleep(2)
@@ -14,5 +14,8 @@ Então("verifico se o texto apareceu na tela com sucesso.") do
   page.assert_text(text, 'Você Clicou no Botão!')
   page.has_text?('Você Clicou no Botão!')
   have_text('Você Clicou no Botão!')
-
+  find('#teste').click
+  assert_no_text(text, 'Você Clicou no Botão!')
+  has_no_text?('Você Clicou no Botão!')
+  sleep(2)
 end
