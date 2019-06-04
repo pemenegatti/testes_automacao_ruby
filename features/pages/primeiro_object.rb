6da8308232415f96506708d444cdf6e8f272d3a1
Pedro Menegatti <pedro.menegatti@centauro.com.br>
2019-06-04 16:24:38 -0300

    page objects

--- features/pages/primeiro_object.rb
+++ features/pages/primeiro_object.rb
@@ -1,7 +1,7 @@
 class PrimeiroObject < SitePrism::Page
     #mapear os elementos
     #fazer nossos metodos
-    @page = PrimeiroObject.new
-    @Page.elemento.click
-    @page.metodo   
+    #@page = PrimeiroObject.new
+    #@Page.elemento.click
+    #@page.metodo   
 end
